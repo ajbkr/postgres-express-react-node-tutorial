588a4fed4ca3b5c8c596836ea4ec488d1a038d20
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-06-07 12:07:38 +0100

    Delete todos and cascade todoitems

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -8,6 +8,21 @@ module.exports = {
     .then(todo => res.status(201).send(todo))
     .catch(error => res.status(400).send(error)),
 
+  destroy: (req, res) => Todo
+    .findByPk(req.params.todoId)
+    .then(todo => {
+      if (!todo) {
+        return res.status(400).send({
+          message: 'Todo Not Found'
+        })
+      }
+      return todo
+        .destroy()
+        .then(() => res.status(204).send())
+        .catch(error => res.status(400).send(error))
+    })
+    .catch(error => res.status(400).send(error)),
+
   list: (req, res) => Todo
     .findAll({
       include: [{
