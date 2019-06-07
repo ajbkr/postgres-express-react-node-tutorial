85a86c677cb55647e4080740b10fa0a9e17c67ab
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-06-07 09:43:55 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const logger = require('morgan')
+
+const app = express()
+
+app.use(logger('dev'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.'
+}))
+
+module.exports = app
